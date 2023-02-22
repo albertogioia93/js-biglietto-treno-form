cbaff4545eda7b11bea58633c05ff62c6667e2c5
albertogioia93 <albertogioia@hotmail.it>
2023-02-22 17:45:09 +0100

    le provo tutte ma niente

--- js/main.js
+++ js/main.js
@@ -1,15 +1,51 @@
 'use strict'
 
-const inputElement = document.getElementById('input-nome');
-const inputButton = document.getElementById('cambia-testo');
+const inputElement = document.getElementById('input-nome').value;
+const inputButton = document.getElementById('genera');
 
-console.log(inputElement.value);
+
 
 
 inputButton.addEventListener('click',
     function(){
-        inputElement.value = 'contenuto dopo click';
-        // alert('prova');
+        console.log(inputElement);
     }
 
 )
+
+
+
+
+
+
+
+
+
+
+// // chiedo all’utente il numero di chilometri che vuole percorrere
+
+// const kmPreferiti = parseInt(prompt('Quanti chilometri vuoi percorrere?'));
+// console.log(kmPreferiti);
+
+// // chiedo all'utente la sua età
+
+// const etaUtente = parseInt(prompt('Inserisci la tua età'));
+// console.log(etaUtente);
+
+// // creo variabile che è risultato tra kmPreferiti e la tariffa di 0,21€ al km
+
+// let prezzoBiglietto = kmPreferiti * 0.21;
+
+
+// // dal risultato del prezzo applico i vari sconti in base all'età
+
+// if(etaUtente < 18) { 
+//     document.querySelector('h1').innerHTML = 'Il prezzo del tuo biglietto è di ' + (prezzoBiglietto - (prezzoBiglietto / 100 * 20)).toFixed(2) + '€';
+//     console.log('Il prezzo del tuo biglietto è di ' + (prezzoBiglietto - (prezzoBiglietto / 100 * 20)).toFixed(2) + '€');    
+// } else if(etaUtente > 65) {
+//     document.querySelector('h1').innerHTML = 'Il prezzo del tuo biglietto è di ' + (prezzoBiglietto - (prezzoBiglietto / 100 * 40)).toFixed(2) + '€';
+//     console.log('Il prezzo del tuo biglietto è di ' + (prezzoBiglietto - (prezzoBiglietto / 100 * 40)).toFixed(2) + '€');
+// } else {
+//     document.querySelector('h1').innerHTML = 'Il prezzo del tuo biglietto è di ' + prezzoBiglietto.toFixed(2) + '€';
+//     console.log('Il prezzo del tuo biglietto è di ' + prezzoBiglietto.toFixed(2) + '€');
+// }
