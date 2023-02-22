789578a10c70abcb1c6b617ad44154b00a5d7342
albertogioia93 <albertogioia@hotmail.it>
2023-02-22 15:20:25 +0100

    prove tra input e button

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 'use strict'
+
+const inputElement = document.getElementById('input-nome');
+const inputButton = document.getElementById('cambia-testo');
+
+console.log(inputElement.value);
+
+
+inputButton.addEventListener('click',
+    function(){
+        inputElement.value = 'contenuto dopo click';
+        // alert('prova');
+    }
+
+)
